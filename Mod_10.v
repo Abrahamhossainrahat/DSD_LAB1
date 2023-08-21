7ab4160b787b94da4e88603b6aa981a1e5ff22d2
Raj <rahathossain1510508260@gmail.com>
2023-08-21 21:51:29 +0600

    first Commit

--- Mod_10.v
+++ Mod_10.v
@@ -9,8 +9,9 @@ module mod__10(q,clk);
 	reg j = 1'b1, k=1'b1;
 	wire clr;
 
-	nand(clr, q[1], q[3]);
+	//nand(clr, q[1], q[3]);
 	JK_flip_flop JKFF0(q[0], q0[0], j, k, clk, clr);
+	nand(clr, q[1], q[3]);
 	JK_flip_flop JKFF1(q[1], q0[1], j, k, q[0], clr);
 	JK_flip_flop JKFF2(q[2], q0[2], j, k, q[1], clr);
 	JK_flip_flop JKFF3(q[3], q0[3], j, k, q[2], clr);
